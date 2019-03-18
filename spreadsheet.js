ce908f2f8a33d89d5bd178cc5e046d6c81dbeb5d
Leonardo Russo <contacto@lrusso.com.ar>
2019-03-18 10:38:58 -0300

    Add files via upload

--- spreadsheet.js
+++ spreadsheet.js
@@ -115,7 +115,7 @@ tinymce.PluginManager.add("spreadsheet", function(editor, url)
 							{
 							if (thousandsSeparator=="1")
 								{
-								resultNumberFinal = formatNumber(resultNumberFinal);
+								resultNumberFinal = formatNumber(resultNumberFinal,decimalsUsed);
 								}
 							result = replaceAll(result,resultNumber,resultNumberFinal);
 
@@ -213,9 +213,18 @@ tinymce.PluginManager.add("spreadsheet", function(editor, url)
 		return str.replace(new RegExp(find, "g"), replace);
 		}
 
-	function formatNumber (num)
+	function formatNumber(num,decimalsUsed)
 		{
-		return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,")
+		num = num.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+
+		if (decimalsUsed>0)
+			{
+			var pattern = /[.].*/g;
+			var match = pattern.exec(num);
+			num = replaceAll(num,match[0],match[0].replace(/\s*,\s*|\s+,/g, ""));
+			}
+
+		return num;
 		}
 
 	function showError(className,parentElement,setDirty)
